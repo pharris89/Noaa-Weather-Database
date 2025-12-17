6f382c07571053ba38168b913a94d2ce5910934d
Paula Harris <120838423+pharris89@users.noreply.github.com>
2025-12-17 16:51:15 -0600

    Add script to extract weather data to CSV
    This script extracts temperature and humidity data from a weather database and writes it to a CSV file, converting temperatures from Celsius to Fahrenheit.

---
+++ ExtractTempAndHumidity2.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 17:18:19 2022
+
+@author: pharr
+"""
+
+# Purpose: Extract temperature, humidity data from weather database into CSV file
+# Name: Paula Harris
+# Date: 04/10/2022
+#   Run BuildWeatherDB2.py to build weather database before running this program
+
+import sqlite3
+
+# convert Celsius temperature to Fahrenheit
+
+
+def convertCtoF(tempC):
+    return (tempC*9.0/5.0) + 32.0
+
+
+# file names for database and output file
+dbFile = "weather.db2"
+output_file_name = 'formatdata3.csv'
+
+# connect to and query weather database and
+dbFile = "weather.db2"
+conn = sqlite3.connect(dbFile)
+# create cursor to execute SQL commands
+cur = conn.cursor()
+selectCmd = """ SELECT temperature, relativeHumidity FROM observations
+                ORDER BY timestamp; """
+cur.execute(selectCmd)
+allRows = cur.fetchall()
+# limit the number of rows output to half
+rowCount = len(allRows)//2  
+rows = allRows[:rowCount]
+
+# write data to output file
+with open(output_file_name, "w+") as outf:
+    outf.write('Celsius,Fahrenheit,Humidity')
+    outf.write('\n')
+    for row in rows:
+        tempC = row[0]
+        if tempC is None:  # handle missing temperature value
+            outf.write(',,')
+        else:
+            tempF = convertCtoF(tempC)
+            outf.write(str(tempC)+',')
+            outf.write(str(tempF)+',')
+        humidity = row[1]
+        if humidity is None:  # handle missing humidity value
+            outf.write('\n')
+        else:
+            # print data to file separated by commas
+            outf.write(str(humidity)+'\n')
